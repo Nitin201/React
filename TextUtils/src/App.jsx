dda4aaddad3fd8fe772a6b03c02d8752423fab5d
Nitin Dodamani <dodamanin8@gmail.com>
2024-04-16 11:56:34 +0530

    toggleMode

--- TextUtils/src/App.jsx
+++ TextUtils/src/App.jsx
@@ -3,13 +3,25 @@ import './App.css'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 //import About from './components/About'
+import React, { useState } from 'react';
+
 
 function App() {
-  
+  const [mode, setMode]=useState('light');
+  const toggleMode = () =>
+  {
+    if(mode === 'light'){
+      setMode('dark');
+    }
+    else
+    {
+      setMode('light');
+    }
+  }
 
   return (
     <>
-      <Navbar title=" Textutilies" about="about Textutlies"/>
+      <Navbar title=" Textutilies" about="about Textutlies" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
       <TextForm heading="enter the text anyalizing below" />
       {/*<About/>*/}
