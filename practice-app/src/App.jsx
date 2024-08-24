dd3b65c8c3747adc7237967c8dd2372369932a29
Nitin Dodamani <dodamanin8@gmail.com>
2024-08-24 19:59:19 +0530

    commited

---
+++ practice-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// eslint-disable-next-line no-unused-vars
+import nit from './components/nit'
+function App() {
+
+
+  return (
+    <>
+      <nit/>
+  
+    </>
+  )
+}
+
+export default App
